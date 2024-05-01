72759aa149c09ee578f6cbe3ef41748abfa38928
ngooci <nmaiphuong235@gmail.com>
2024-05-01 16:09:54 -0400

    Add project files.

---
+++ CateringManagement/Controllers/CustomerController.cs
@@ -0,0 +1,297 @@
+﻿using CateringManagement.CustomControllers;
+using CateringManagement.Data;
+using CateringManagement.Models;
+using CateringManagement.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using String = System.String;
+
+namespace CateringManagement.Controllers
+{
+    public class CustomerController : ElephantController
+    {
+        private readonly CateringContext _context;
+
+        public CustomerController(CateringContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Customers
+        public async Task<IActionResult> Index(int? page, int? pageSizeID, 
+            string actionButton, string sortDirection = "asc", string sortField = "Customer")
+        {
+            //List of sort options.
+            //NOTE: make sure this array has matching values to the column headings
+            string[] sortOptions = new[] { "Customer", "Company Name", "Phone", "Customer Code" };
+
+            var customers = _context.Customers
+                .AsNoTracking();
+
+            //Before we sort, see if we have called for a change of filtering or sorting
+            if (!String.IsNullOrEmpty(actionButton)) //Form Submitted!
+            {
+                page = 1;//Reset page to start
+
+                if (sortOptions.Contains(actionButton))//Change of sort is requested
+                {
+                    if (actionButton == sortField) //Reverse order on same field
+                    {
+                        sortDirection = sortDirection == "asc" ? "desc" : "asc";
+                    }
+                    sortField = actionButton;//Sort by the button clicked
+                }
+            }
+            //Now we know which field and direction to sort by
+            if (sortField == "Company Name")
+            {
+                if (sortDirection == "asc")
+                {
+                    customers = customers
+                        .OrderBy(p => p.CompanyName);
+                }
+                else
+                {
+                    customers = customers
+                        .OrderByDescending(p => p.CompanyName);
+                }
+            }
+            else if (sortField == "Phone")
+            {
+                if (sortDirection == "asc")
+                {
+                    customers = customers
+                        .OrderBy(p => p.Phone);
+                }
+                else
+                {
+                    customers = customers
+                        .OrderByDescending(p => p.Phone);
+                }
+            }
+            else if (sortField == "Customer Code")
+            {
+                if (sortDirection == "asc")
+                {
+                    customers = customers
+                        .OrderBy(p => p.CustomerCode);
+                }
+                else
+                {
+                    customers = customers
+                        .OrderByDescending(p => p.CustomerCode);
+                }
+            }
+            else //Sorting by Customer
+            {
+                if (sortDirection == "asc")
+                {
+                    customers = customers
+                        .OrderBy(p => p.LastName)
+                        .ThenBy(p => p.FirstName);
+                }
+                else
+                {
+                    customers = customers
+                        .OrderByDescending(p => p.LastName)
+                        .ThenByDescending(p => p.FirstName);
+                }
+            }
+            //Set sort for next time
+            ViewData["sortField"] = sortField;
+            ViewData["sortDirection"] = sortDirection;
+
+            //Handle Paging
+            int pageSize = PageSizeHelper.SetPageSize(HttpContext, pageSizeID, ControllerName());
+            ViewData["pageSizeID"] = PageSizeHelper.PageSizeList(pageSize);
+            var pagedData = await PaginatedList<Customer>.CreateAsync(customers.AsNoTracking(), page ?? 1, pageSize);
+
+            return View(pagedData);
+        }
+
+        // GET: Customers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .Include(c => c.Functions)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,FirstName,MiddleName,LastName,CompanyName,Phone,CustomerCode")] Customer customer)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(customer);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Details", new { customer.ID });
+                }
+            }
+            catch (DbUpdateException dex)
+            {
+                if (dex.GetBaseException().Message.Contains("UNIQUE constraint failed: Customers.CustomerCode"))
+                {
+                    ModelState.AddModelError("CustomerCode", "Unable to save changes. Remember, you cannot have duplicate Customer Codes.");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+                }
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id)
+        {
+            //Go get the customer to update
+            var customerToUpdate = await _context.Customers.FirstOrDefaultAsync(p => p.ID == id);
+
+            //Check that we got the customer or exit with a not found error
+            if (customerToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            //Try updating it with the values posted
+            if (await TryUpdateModelAsync<Customer>(customerToUpdate, "",
+                c => c.FirstName, c => c.MiddleName, c => c.LastName, c => c.CompanyName,
+                c => c.Phone, c => c.CustomerCode))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Details", new { customerToUpdate.ID });
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customerToUpdate.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                catch (DbUpdateException dex)
+                {
+                    if (dex.GetBaseException().Message.Contains("UNIQUE constraint failed: Customers.CustomerCode"))
+                    {
+                        ModelState.AddModelError("CustomerCode", "Unable to save changes. Remember, you cannot have duplicate Customer Codes.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+                    }
+                }
+            }
+            return View(customerToUpdate);
+        }
+
+        // GET: Customers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Customers == null)
+            {
+                return Problem("There are no Customers to delete.");
+            }
+            var customer = await _context.Customers.FindAsync(id);
+            try
+            {
+                if (customer != null)
+                {
+                    _context.Customers.Remove(customer);
+                }
+                await _context.SaveChangesAsync();
+                return Redirect(ViewData["returnURL"].ToString());
+            }
+            catch (DbUpdateException dex)
+            {
+                if (dex.GetBaseException().Message.Contains("FOREIGN KEY constraint failed"))
+                {
+                    ModelState.AddModelError("", "Unable to Delete Customer. Remember, you cannot delete a Customer that has a function in the system.");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+                }
+            }
+            return View(customer);
+
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customers.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ CateringManagement/Data/ExtraMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CateringManagement.Data
+{
+    public static class ExtraMigration
+    {
+        public static void Steps(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"
+                    CREATE TRIGGER SetFunctionTimestampOnUpdate
+                    AFTER UPDATE ON Functions
+                    BEGIN
+                        UPDATE Functions
+                        SET RowVersion = randomblob(8)
+                        WHERE rowid = NEW.rowid;
+                    END
+                ");
+            migrationBuilder.Sql(
+                @"
+                    CREATE TRIGGER SetFunctionTimestampOnInsert
+                    AFTER INSERT ON Functions
+                    BEGIN
+                        UPDATE Functions
+                        SET RowVersion = randomblob(8)
+                        WHERE rowid = NEW.rowid;
+                    END
+                ");
+        }
+    }
+}
